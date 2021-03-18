d4b3a9c645d46782ca16896a4407855b68ef44ff
manubumb <46905345+manubumb@users.noreply.github.com>
2021-03-18 17:19:49 -0400

    Initial commit
    This file contains the lifehacks

---
+++ lambda/hacks.js
@@ -0,0 +1,12 @@
+module.exports = [
+'To make a room smell better, tape a dryer sheet over your AC unit and turn it on.',
+'To keep clothes from wrinkling when you travel, roll them instead of fold them in your suitcase.',
+'On a hot day, use a coozie to cover your car\'s gear shift to keep it cool.',
+'During a trip to the beach, store your keys and money in an old (cleaned) sunscreen bottle.',
+'To serve condiments at a BBQ, use a muffin tin pan rather than bowls.',
+'Too many keys?  Use fingernail polish to paint them different colors.',
+'To clean a blender, add water and soap, and turn it on.',
+'To keep pizza hot on the drive home, turn on your seat warmer.',
+'At a hotel and forgot your charger?  The TV usually has a USB plug-in.',
+'On your phone, use an accented letter in your passcode.  It\'ll be harder to guess.',
+];
